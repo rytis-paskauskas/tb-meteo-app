8ed48f0ef5feb04a09405c6a884c0387a33f7fe8
Rytis Paškauskas <rytis.paskauskas@gmail.com>
2026-02-26 10:57:10 +0100

    Release 0.1.1: Fixed failure to save data

--- app/cli.py
+++ app/cli.py
@@ -77,7 +77,6 @@ def main():
 
     beg_ts = pd.Timestamp(beg_dt).tz_localize(config['timezone']).tz_convert("UTC").timestamp()
     end_ts = pd.Timestamp(end_dt).tz_localize(config['timezone']).tz_convert("UTC").timestamp()
-    breakpoint()
     item = VALID_IDENTIFIERS[args.identifier]
     entity = EntityId(item['device_id'], item['device_type'])
     tb_opts = {'limit': (1+days)*144, 'order_by': 'ASC', 'use_strict_data_types': True}
